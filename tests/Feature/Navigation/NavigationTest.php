2e82c871e8abcac61a9e9bd67b85ee47b6b02c9e
Samuel Cuenca <ramirezcuenca@outlook.es>
2024-01-16 18:13:32 -0600

    Rendering livewire component

---
+++ tests/Feature/Navigation/NavigationTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature\Navigation;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class NavigationTest extends TestCase
+{
+    /**
+     * @test
+     * @return void
+     */
+    public function navigation_component_can_be_rendered()
+    {
+        $this->get('/')->assertStatus(200)
+                        ->assertSeeLivewire('navigation.navigation');
+    }
+
+}
